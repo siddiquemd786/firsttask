d74aac6f492f6adb5d767fc818a3339dcd8fbc84
siddique <siddiquemd161@gmail.com>
2024-05-31 05:47:18 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,111 @@
+// 1 no. question answar.
+
+
+let carName = "Volvo" ; 
+
+console.log (carName)
+
+// 2 no. question answar.
+
+const person = {firstName:"John", lastName:"Doe", age:50};
+
+console.log(person)
+// 3 no. question answar.
+var x = 10;
+var y = 5;
+
+var x=x*y
+
+console.log(x)
+
+// 4 no. question answar.
+
+
+let length = 16; // data type number.
+console.log(typeof length);
+let lastName = "Johnson"; // data type string. 
+console.log(typeof lastName); 
+const xx = {
+  firstName: "John",  
+  lastName: "Doe"
+};    // data type object.
+console.log(typeof xx);
+
+// 5 no. question answar.
+ 
+ function myFunction() {
+  alert("Hello World!");
+}
+
+myFunction();
+
+
+// 6 no. question answar.
+
+    let person1 = {
+        firstName:"John",
+         age:50, 
+      }
+
+      let message = person1.firstName + " is " + person1.age
+
+      alert(message);
+
+
+// 7 no. question answar.
+
+      function sayHello() {
+        alert("Bangladesh");
+    }
+      
+// 8 no. question answar.
+// 8.1 no. question answar.
+    const cars = ["Volvo", "Jeep", "Mercedes"];
+
+    alert(cars.length)
+
+// 8.2 no. question answar.
+    const Brand = ["Volvo", "Jeep", "Mercedes"]; 
+
+    Brand [0] = "ford"
+
+    console.log(Brand)
+
+    // 9 no. question answar.
+
+    // 9.1 no. question answar.
+    console.log (Math.round(Math.random()*8))
+   
+    // 9.2 no. question answar.
+    var a = [10,20]
+    function largest (a){
+        return (Math.max(...a))
+    }
+    console.log(largest(a))
+
+     // 9.3 no. question answar.
+     console.log(Math.sqrt(9))
+
+    // 10 no. question answar.
+
+    // 10.1 no. question answar.
+
+    x = 10 ;
+    y = 5;
+
+    if ( x > y ) {
+        alert ("true")
+    }
+// 10.2 no. question answar.
+    let age = 15; // Example age
+
+age < 18 ? alert("Too young") : alert("Old enough");
+
+
+
+
+
+
+
+
+
